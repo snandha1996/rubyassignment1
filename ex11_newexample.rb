a2659cc15acc55300b01fcca097f1d4a7676a2b9
snandha1996 <s.nandha1996@gmail.com>
2021-01-24 12:06:01 +0530

    Create ex11_newexample.rb

---
+++ ex11_newexample.rb
@@ -0,0 +1,7 @@
+puts "Hello"
+print "Your name:"
+name = gets
+print "Which block would you like to visit?"
+block = gets.chomp.to_i
+
+print "Thank you #{name} You are visiting block #{block} Have a good day"
