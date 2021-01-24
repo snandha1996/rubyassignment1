82bf21bf382437b28201d6359254a51068e488f7
snandha1996 <s.nandha1996@gmail.com>
2021-01-24 11:01:31 +0530

    Add files via upload

---
+++ ex3_1.rb
@@ -0,0 +1,10 @@
+Nandha = 10
+Subash = 20
+Nijandhan = 30
+
+puts "Nandha scored #{Nandha}"
+puts "Subash scored #{Subash}"
+puts "Nijandhan scored #{Nijandhan}"
+
+puts"--------------------"
+puts "Total Score: #{Nandha + Subash + Nijandhan}"
---
+++ ex6.rb
@@ -0,0 +1,39 @@
+#an integer value is assigned to a variable
+types_of_people = 10
+#a string is assigned to a variable and also value of integer variable using #{}
+x = "There are #{types_of_people} types of people." #a string is put inside a string
+#a string is assigned to a variable
+binary = "binary"
+#a string is assigned to a variable
+do_not = "don't"
+#a string is assigned to a variable and also value of 2 other string variables using #{}
+y = "Those who know #{binary} and those who #{do_not}." #a string is put inside a string
+
+#prints x
+puts x
+#prints y
+puts y
+
+#prints a string along with value of a variable x using #{}
+puts "I said: #{x}." #a string is put inside a string
+#prints a string along with value of a variable y using #{}
+puts "I also said: '#{y}'." #a string is put inside a string
+
+#boolean value is assigned to a variable
+hilarious = false
+#a string is assigned to a variable and also value a boolean variables using #{}
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}" #a string is put inside a string
+
+#prints joke_evaluation
+puts joke_evaluation
+
+#a string is assigned
+w = "This is the left side of..."
+#a string is assigned
+e = "a string with a right side."
+
+#cancadinate 2 strings  // w and e
+#both variables are of strings hence the + operator merged 2 strings
+puts w + e
+
+#Single quotes and double quotes behave in exactly the same way
